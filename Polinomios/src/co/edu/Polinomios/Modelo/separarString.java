400a97edbf8171d1af4c52008081f405c1f5d1e9
JuanMosquera <jm1128un@gmail.com>
2017-04-01 19:07:32 -0500

    correccion

--- Polinomios/src/co/edu/Polinomios/Modelo/separarString.java
+++ Polinomios/src/co/edu/Polinomios/Modelo/separarString.java
@@ -12,12 +12,15 @@ package co.edu.Polinomios.Modelo;
 public class separarString {
     
     public static void main(String[] args) {
-        String polinomio="3x5 - .5x3 - 3x2 + 5x +2x +1";
+        
+        String polinomio=" x -3x2 -.5x3 -3.1x5 +2x +2 +x +x2";
+        
         String[] letras;
-        double d;
-        int p;
-        char s;
+        int c;
+        int j;
+        
         polinomio = polinomio.toLowerCase();
+        polinomio = polinomio.replace("  ", "");
         polinomio = polinomio.replace(" ", "");
         System.out.println(polinomio);
 
@@ -33,88 +36,81 @@ public class separarString {
         }
         
         System.out.println("prueba");
-        int c=(fracciones.length)*2;
+        c=(fracciones.length)*2;
         letras= new String[c];
-        int j =0;
+        j =0;
         
         
         
         if (fracciones[0].equals("")){
-                    for(int i=1;i<fracciones.length;i++)
-                {
-                     String[] digito = fracciones[i].split("x");
+            
+            for (int i=1;i<fracciones.length;i++) {
+               String[] digito = fracciones[i].split("x");
         //                     System.out.println(fracciones[i]);
-                              System.out.println(digito[0]);
+              // System.out.println(digito[0]);
 
-                            letras[j]=digito[0];
-                            j++;
-                            if(digito.length==2) {
-                              System.out.println(digito[1]);
-                              letras[j]=digito[1];
-                              j++;
-                            }
-
+                if (digito.length==0) {
+                    
+                    letras[j]="1";
+                    j++;
+                    letras[j]="1";
+                    j++;
+                            
+                } else {
+                    letras[j]=digito[0];
+                    j++;
+                    if(digito.length==2) {
+                        System.out.println(digito[1]);
+                        letras[j]=digito[1];
+                        j++;
+                    }else{
+                        letras[j]="1";
+                        j++;
+                    }        
                 }
+            }
         } else {
-                          for(int i=0;i<fracciones.length;i++)
-                {
-                     String[] digito = fracciones[i].split("x");
+            
+            for (int i=0;i<fracciones.length;i++) {
+               String[] digito = fracciones[i].split("x");
         //                     System.out.println(fracciones[i]);
-                              System.out.println(digito[0]);
+               // System.out.println(digito[0]);
 
-                            letras[j]=digito[0];
-                            j++;
-                            if(digito.length==2) {
-                              System.out.println(digito[1]);
-                              letras[j]=digito[1];
-                              j++;
-                            }
-
+                if (digito.length==0) {
+                    
+                    letras[j]="1";
+                    j++;
+                    letras[j]="1";
+                    j++;
+                            
+                } else {
+                    letras[j]=digito[0];
+                    j++;
+                    if(digito.length==2) {
+                        System.out.println(digito[1]);
+                        letras[j]=digito[1];
+                        j++;
+                    }else{
+                        letras[j]="1";
+                        j++;
+                    }        
                 }
+                          
+            }
         }
+        
+        System.out.println("letras");
+         for(int i=0;i<letras.length;i++)
+        {
+            if (letras[i]!=null) {
+                 System.out.println(letras[i]);
+            }else{
+                break;
+            }
+           
+        }
+        
    }
         
-        
-        
-//        int c=(fracciones.length)*2;
-//        letras= new String[c];
-//        System.out.println("resultado");
-//        
-//        int j =0;
-//        letras[j]=fracciones[0];
-//        j++;
-//        
-//        
-//            for (int i = 1; i < fracciones.length; i++) {
-//    //            System.out.println(i);
-//
-//
-//
-//                    String[] digito = fracciones[i].split(" ");
-//                    letras[j]=digito[0];
-//                    j++;
-//                    letras[j]=digito[1];
-//                    j++;
-//                    System.out.println(digito[0]);
-//                    System.out.println(digito[1]);
-//
-//
-//            }    
-//        
-//        
-//        
-//                
-//             
-//        System.out.println("final");
-//        for (int i = 0; i < letras.length; i++) {
-//            System.out.println(letras[i]);
-//        }
-//       
-//       System.out.println(fracciones.length);
-////        for(int i=0;i<fracciones.length;i++)
-////        {
-////            String[] digito = fracciones[i].split(" ");
-////            System.out.println(digito[i]);
-////        }
     
 }
